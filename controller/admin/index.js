429e7abb50a7676f001322412175801ad0823c56
karambeer <karambeer.singh@paxcel.net>
2016-09-11 15:24:14 +0530

    rectify controller

--- controller/admin/index.js
+++ controller/admin/index.js
@@ -4,4 +4,5 @@ app.use('/users',require('./UserController.js'));
 app.use('/tests',require('./TestController.js'));
 app.use('/labs',require('./LabController.js'));
 app.use('/categories',require('./CategoryController.js'));
+app.use('/shoppingcart',require('./ShoppingCartController.js'));
 module.exports = app;
