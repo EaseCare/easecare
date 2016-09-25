97ee0a83a8a2b6d9354d885c038479b7aea6dfcc
karambeer <karambeer.singh@paxcel.net>
2016-09-25 20:01:46 +0530

    fix service

--- dao/admin/OrderDao.js
+++ dao/admin/OrderDao.js
@@ -5,7 +5,7 @@ var cache = require('service/cache').local;
 var logger = require('helper/Logger.js')(moduleName);
 
 var connection = require('service/mysql/Pool.js');
-
+var utilDao = require('dao/util/UtilDao.js');
 var OrderDao = function () { }
 
 OrderDao.prototype.add = function(data, cb){
