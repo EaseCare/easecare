c4a71be399e310c4ff2dd4a23406a7a5a4738b35
karambeer <karambeer.singh@paxcel.net>
2016-12-18 02:12:46 +0530

    update lab response

--- responser/LabResponser.js
+++ responser/LabResponser.js
@@ -8,9 +8,53 @@ var messages = config.messages;
 
 var LabResponser = function () { };
 
-LabResponser.prototype.getListResponse = function (lab_list, cb) {
-    
+LabResponser.prototype.getListForTestResponse = function (lab_test_list) {
+    var self = this;
+    var responseList = [];
+    lab_test_list.forEach(function(lab_test){
+        var response = {};
+        var test_ids = (lab_test.test_ids).split(",");
+        var test_names = (lab_test.test_name).split(",");
+        var test_prices = (lab_test.test_price).split(",");
+        var tests = self.MapObjects(test_ids, test_names, test_prices);
+        response.id = lab_test.id;
+        response.name = lab_test.name;
+        response.lab_image = lab_test.lab_image;
+        response.address_line_1 = lab_test.address_line_1;
+        response.address_line_2 = lab_test.address_line_2;
+        response.landmark = lab_test.landmark;
+        response.city = lab_test.city;
+        response.state = lab_test.state;
+        response.latitude = lab_test.latitude;
+        response.longitude = lab_test.longitude
+        response.tests = tests;
+        response.total_price = lab_test.total_price;
+        responseList.push(response);
+    })
+    return responseList;
     
 }
-
-module.exports = LabService;
+LabResponser.prototype.MapObjects = function(array1, array2, array3){
+    var length = 0;
+    var tests = [];
+    if(array1.length < array2.length){
+        if(array1.length <array3.length){
+            length = array1.length;
+        }else{
+            length = array3.length;
+        }
+    }else if(array2.length < array3.length){
+        length = array2.length;
+    }else{
+        length = array3.length;
+    }
+    for(var i=0; i<length; i++){
+        var test = {};
+        test.id = array1[i];
+        test.name = array2[i];
+        test.price = array3[i];
+        tests.push(test);
+    }
+    return tests;
+}
+module.exports = LabResponser;
