072f2b80b3259a9830a81093fa8831a99b3e6f2d
karambeer <karambeer.singh@paxcel.net>
2017-03-09 00:32:41 +0530

    add paytm functionallity

--- controller/admin/PaymentController.js
+++ controller/admin/PaymentController.js
@@ -5,6 +5,8 @@ var config = require('config');
 var response = require('controller/ResponseController.js');
 var moduleName = __filename;
 var logger = require("helper/Logger.js")(moduleName);
+var paytm_config = require('paytm/paytm_config').paytm_config;
+var paytm_checksum = require('paytm/checksum');
 var constants = config.orionConstants;
 var messages = config.messages;
 var responseCodes = config.responseCode;
@@ -34,4 +36,32 @@ app.get('/orders', function (req, res) {
     });
 });
 
+app.post('/paytm/generate_checksum', function(req, res){
+    logger.info("Paytm generate checksum request recieved");
+    paytm_checksum.genchecksum(req.data, paytm_config.MERCHANT_KEY, function (err, data) {
+        if(err){
+            return response("Something wrong",500,null,res);
+        }else{
+            var finalResponse = {};
+            finalResponse.CHECKSUMHASH = data.CHECKSUMHASH;
+        }
+        return response(err, 200, finalResponse, res);
+    });
+});
+
+app.post('/paytm/verify_checksum', function(req, res){
+    logger.info("Paytm verify checksum request recieved");
+    var decodedBody = req.data;
+    var isCheckSumValid = false;
+    if(paytm_checksum.verifychecksum(decodedBody, paytm_config.MERCHANT_KEY)) {
+        isCheckSumValid = true;
+    }
+    /*if(decodedBody.CHECKSUMHASH){
+        delete decodedBody.CHECKSUMHASH;
+    }*/
+    var returnData = {
+        isCheckSumValid : isCheckSumValid
+    }
+     return response(null, 200, returnData, res)
+});
 module.exports = app;
---
+++ paytm/paytm_config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  paytm_config: {
+		MID: 'EaseCa82880198246172',
+		WEBSITE: 'www.easecares.com',
+    CHANNEL_ID: 'WAP',
+    INDUSTRY_TYPE_ID: '',
+    MERCHANT_KEY : 'EaseCa8288019824'
+	}
+}
